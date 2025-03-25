7622e62eb9bf2a571dff39aab26f400dd45faa25
olemgl <olemgl@stud.ntnu.no>
2025-03-25 17:02:58 +0100

    Added 2 of 3 API endpoints

--- utils/consts.go
+++ utils/consts.go
@@ -1,7 +1,13 @@
 package utils
 
+// Paths
 const ROOT_PATH = "/"
 const REGISTRATION_PATH = "/dashboard/v1/registrations/"
 const DASHBOARD_PATH = "/dashboard/v1/dashboards/"
 const NOTIFICATION_PATH = "/dashboard/v1/notifications/"
 const STATUS_PATH = "/dashboard/v1/status/"
+
+// api's
+const RESTCountriesAPI = "http://129.241.150.113:8080/v3.1"
+//TODO: add the metro API
+const CurrencyAPI = "http://129.241.150.113:9090/currency/"
