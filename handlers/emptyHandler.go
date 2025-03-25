7a928ab3f62ee242f630f92718c8260a04253df5
olemgl <olemgl@stud.ntnu.no>
2025-03-25 16:37:14 +0100

    completed root handler

--- handlers/emptyHandler.go
+++ handlers/emptyHandler.go
@@ -1,2 +1,24 @@
 package handlers
 
+import (
+	"assigment2/utils"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func RootPath(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", "text/html")
+
+	output := "This service provides functionality on these paths: <br>" +
+		"<a href=\"" + utils.REGISTRATION_PATH + "\">" + utils.REGISTRATION_PATH + "<br>" +
+		"<a href=\"" + utils.DASHBOARD_PATH + "\">" + utils.DASHBOARD_PATH + "<br>" +
+		"<a href=\"" + utils.NOTIFICATION_PATH + "\">" + utils.NOTIFICATION_PATH + "<br>" +
+		"<a href=\"" + utils.STATUS_PATH + "\">" + utils.STATUS_PATH
+
+	_, err := fmt.Fprint(w, output)
+	if err != nil {
+		log.Println("Error when returning root output: ", err)
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+	}
+}
--- main.go
+++ main.go
@@ -1,18 +1,21 @@
 package main
 
 import (
+	"assigment2/handlers"
+	"assigment2/utils"
 	"log"
 	"net/http"
 	"os"
 )
 
-func main(){
+func main() {
 	port := os.Getenv("PORT")
 	if port == "" {
 		log.Println("Port has not been set, using default 8080")
 		port = "8080"
 	}
+	http.HandleFunc(utils.ROOT_PATH, handlers.RootPath)
 
-	log.Println("Starting server on port: "+ port +"..." )
-	log.Fatal(http.ListenAndServe(":" + port, nil))
+	log.Println("Starting server on port: " + port + "...")
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
