939d8f0028f11d570364e2f9fcabd2bfff5d2fd4
maeilert <maeilert@stud.ntnu.no>
2025-03-26 12:19:58 +0100

    Added firestore skeleton + firestore database is set up

--- main.go
+++ main.go
@@ -3,12 +3,38 @@ package main
 import (
 	"assigment2/handlers"
 	"assigment2/utils"
+	"cloud.google.com/go/firestore"
+	"context"
+	firebase "firebase.google.com/go"
+	"google.golang.org/api/option"
 	"log"
 	"net/http"
 	"os"
 )
 
+var ctx context.Context
+var client *firestore.Client
+
 func main() {
+
+	ctx = context.Background()
+
+	opt := option.WithCredentialsFile("api-keys/serviceAccountKey.json") // API KEY NEEDS TO BE LOCAL! - and added to .gitignore!
+	app, err := firebase.NewApp(ctx, nil, opt)
+	if err != nil {
+		log.Printf("error initializing app: %v", err)
+		return
+	}
+
+	client, err := app.Firestore(ctx)
+
+	if err != nil {
+		log.Printf("Error initializing firestore: %v", err)
+		return
+	}
+
+	defer client.Close()
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		log.Println("Port has not been set, using default 8080")
