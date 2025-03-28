04c7ebc3c94970e1b4a0c09f49ad3514f3934f52
Magnus Dybdal <magndy@stud.ntnu.no>
2025-03-28 22:53:42 +0100

    Made struct for dashboard configurations

--- utils/structs.go
+++ utils/structs.go
@@ -1,2 +1,18 @@
 package utils
 
+/*
+*	Struct for dashboard configuration.
+ */
+type DashboardConfig struct {
+	Country string					`firestore:"country" json:"country"`
+	IsoCode string					`firestore:"isoCode" json:"isoCode"`
+	Features struct {
+		Temperature bool			`firestore:"temperature" json:"temperature"`
+		Precipiation bool			`firestore:"percipiation" json:"precipiation"`
+		Capital bool				`firestore:"capital" json:"capital"`
+		Coordinates bool			`firestore:"coordinates" json:"coordinates"`
+		Population bool				`firestore:"population" json:"population"`
+		Area bool					`firestore:"area" json:"area"`
+		TargetCurrencies []string 	`firestore:"targetCurrencies" json:"targetCurrencies"`
+	}								`firestore:"features" json:"features"`
+}
